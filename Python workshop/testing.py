005efeeae74b330c2763b80ccdc895362e36d40b
addhyan94 <addhyant85@gmail.com>
2025-05-31 13:40:42 +0530

    .....

---
+++ Python workshop/testing.py
@@ -0,0 +1,18 @@
+# Flow control ke liye ....
+'''
+    Write a python program to find greatest number in two numbers.
+'''
+import time
+a=eval(input("Enter First number = "))
+b=eval(input("Enter Second Number = "))
+time.sleep(2)
+print("\n Chalo aapan check karte hai ki jo aapne number dala hai bo \n \t Greatest hai ya nahi .....")
+time.sleep(3)
+print("\n\t\t Ruko jara raja ji ho raha hai lload hone do time lagta hai computer ko.. khikhikhikhi...")
+time.sleep(8)
+if a>b:
+    print(a,"is a greatest ")
+elif b>a:
+    print(b,"is a greatest ")
+else:
+    print("Aabe tu pagal ho gaya kya number dal na kya dal raha hai tu .....")
