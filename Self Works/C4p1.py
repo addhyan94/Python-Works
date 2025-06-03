9b7a04310767445ef268eca5822b9e234eddc8a0
addhyan94 <addhyant85@gmail.com>
2025-06-03 19:12:46 +0530

    Chapter 4 practice Set

--- Self Works/C4p1.py
+++ Self Works/C4p1.py
@@ -1,2 +1,18 @@
 # WAP to store seven fruits in a list entered bt the user.
 
+fruit = []
+f1 = input ("Enter fruit name : ")
+fruit.append(f1)
+f2 = input ("Enter fruit name : ")
+fruit.append(f2)
+f3 = input ("Enter fruit name : ")
+fruit.append(f3)
+f4 = input ("Enter fruit name : ")
+fruit.append(f4)
+f5 = input ("Enter fruit name : ")
+fruit.append(f5)
+f6 = input ("Enter fruit name : ")
+fruit.append(f6)
+f7 = input ("Enter fruit name : ")
+fruit.append(f7)
+print(fruit)
