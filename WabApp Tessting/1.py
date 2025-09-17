a96789f9021f4dc4d6106daf42bc5d0cd844debf
addhyan94 <addhyant85@gmail.com>
2025-09-17 22:31:40 +0530

    Demo 1

--- WabApp Tessting/1.py
+++ WabApp Tessting/1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, bro! This is my first Webpage app .'
+    return 'Hello, bro! This is my first Webpage app.'
 
 if __name__ == '__main__':
     app.run(debug=True)
