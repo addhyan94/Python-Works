dd868cf4ff588b0d225579df3b252de5e21fb7de
addhyan94 <addhyant85@gmail.com>
2025-10-09 17:29:36 +0530

    Testing 1

---
+++ WabApp Tessting/new.dart
@@ -0,0 +1,26 @@
+// String
+var name ="Tiwari JI "
+// int number 
+var age = 20;
+// boolean
+var isMail = true;
+var isminor = (age > 18);// -> true
+
+// list
+var fruits = ["apple","banana","orange"];
+
+var orange = fruits[2]; // -> replace banana to orange 
+
+// maps / objects 
+
+// key/value pairs
+var restaurant = {
+  "name" ; "Tiwari's Hotel ",
+  "location" : "Jhansi",
+  "rating" : 4.8
+};
+var person ={
+  "name" : "Tiwari Ji ",
+  "age" : 20,
+  "isMale" : true
+};
